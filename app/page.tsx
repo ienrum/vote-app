6ec9f68e575243dd74d09ea88f0f05b020c57ce0
ienrum <azang0506@gmail.com>
2024-05-27 12:37:56 +0900

    feat: home page 서 auth 안될시 redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { createClient } from "@/lib/supabase/server";
 import Link from "next/link";
-export default function Home() {
+export default async function Home() {
+  const { error } = await createClient().auth.getUser();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
   return (
     <>
-      <CardTitle>일정 투표 사이트</CardTitle>
+      <CardTitle>투표 사이트</CardTitle>
       <CardDescription className="mt-4">
-        사용자가 간편하게 일정을 조율하고 투표할 수 있도록 돕는 서비스입니다.
+        사용자가 간편하게 투표할 수 있도록 돕는 서비스입니다.
       </CardDescription>
       <Link href="/create-vote">
         <Button className="mt-4">투표 생성</Button>
