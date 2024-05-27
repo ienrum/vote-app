4704aeacd9c86a00d96849403d449c7c9386ffd7
ienrum <azang0506@gmail.com>
2024-05-27 11:45:46 +0900

    style: result 스타일 수정

--- app/vote/[id]/_components/Result.tsx
+++ app/vote/[id]/_components/Result.tsx
@@ -1,4 +1,6 @@
 import { getVoteOptions } from "@/app/apis/vote";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 interface VoteResultProps {
   voteId: string;
@@ -6,20 +8,37 @@ interface VoteResultProps {
 
 const VoteResult = async ({ voteId }: VoteResultProps) => {
   const { data: options, error } = await getVoteOptions(voteId);
+  const totalCount = options?.reduce((acc, { count }) => acc + (count ?? 0), 0);
+  const percentageOf = (count: number) =>
+    `${((count / (totalCount ?? 1)) * 100).toFixed(0)}%`;
 
   if (error) {
     throw new Error(error.message);
   }
 
   return (
-    <>
-      {options?.map(({ id, value, count }) => (
-        <li key={id}>
-          {value}
-          <p>{count}</p>
-        </li>
-      ))}
-    </>
+    <div>
+      <ul className="flex flex-col gap-4">
+        {options?.map(({ id, value, count }) => (
+          <li key={id} className="flex justify-start items-center ">
+            <p
+              className="w-full bg-slate-white h-12 flex items-center px-2 rounded-md  border-slate-300 border-2 text-slate-800 justify-between bg-slate-500"
+              style={{
+                background: `linear-gradient(to right, rgb(100 116 139 / var(--tw-bg-opacity)) ${percentageOf(
+                  count ?? 0
+                )}, #e2e8f0 ${percentageOf(count ?? 0)})`,
+              }}
+            >
+              <span>{value}</span>
+              {percentageOf(count ?? 0)}
+            </p>
+          </li>
+        ))}
+      </ul>
+      <Link href="/">
+        <Button className="mt-4">돌아가기</Button>
+      </Link>
+    </div>
   );
 };
 
