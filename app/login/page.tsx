a9e65907946007162ae93aed9981c8606b7b838e
ienrum <azang0506@gmail.com>
2024-05-27 12:45:18 +0900

    style: 로그인 페이지 스타일 변경

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,21 +7,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col gap-4">
       <CardTitle>일정 투표 사이트</CardTitle>
       <CardDescription className="mt-4">
         사용자가 간편하게 일정을 조율하고 투표할 수 있도록 돕는 서비스입니다.
       </CardDescription>
-      <CardFooter className="flex justify-center py-10">
-        <Image
-          src={kakoImage}
-          alt="plus"
-          className=" cursor-pointer"
-          onClick={() => {
-            signInWithKakao();
-          }}
-        />
-      </CardFooter>
-    </>
+      <Image
+        src={kakoImage}
+        alt="plus"
+        className=" cursor-pointer"
+        onClick={() => {
+          signInWithKakao();
+        }}
+      />
+    </div>
   );
 }
