de5062ae6e00dbd304f60caad8fa394766803048
ienrum <azang0506@gmail.com>
2024-05-27 10:57:56 +0900

    style: login 페이지 스타일 수정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,27 @@
 "use client";
 
 import { signInWithKakao } from "@/app/apis/auth";
-import { Card } from "@/components/ui/card";
+import { CardDescription, CardFooter, CardTitle } from "@/components/ui/card";
 import kakoImage from "@/public/kakao_login.png";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Card className="mt-8">
-      <h1>일정 투표 사이트</h1>
-      <h3 className="mt-4">
+    <>
+      <CardTitle>일정 투표 사이트</CardTitle>
+      <CardDescription className="mt-4">
         사용자가 간편하게 일정을 조율하고 투표할 수 있도록 돕는 서비스입니다.
-      </h3>
-      <Image
-        src={kakoImage}
-        alt="plus"
-        className=" cursor-pointer"
-        onClick={() => {
-          signInWithKakao();
-        }}
-      />
-    </Card>
+      </CardDescription>
+      <CardFooter className="flex justify-center py-10">
+        <Image
+          src={kakoImage}
+          alt="plus"
+          className=" cursor-pointer"
+          onClick={() => {
+            signInWithKakao();
+          }}
+        />
+      </CardFooter>
+    </>
   );
 }
