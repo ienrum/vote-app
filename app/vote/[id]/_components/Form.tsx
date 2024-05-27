3eb4c563467ac4bcf12377731333a541fd86a3af
ienrum <azang0506@gmail.com>
2024-05-27 11:32:28 +0900

    style: vote form 스타일 변경

--- app/vote/[id]/_components/Form.tsx
+++ app/vote/[id]/_components/Form.tsx
@@ -1,7 +1,7 @@
 import { getVote, getVoteOptions } from "@/app/apis/vote";
 import { submitVote } from "@/app/vote/[id]/actions";
 import { Button } from "@/components/ui/button";
-import { CardTitle } from "@/components/ui/card";
+import { CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
@@ -25,14 +25,28 @@ export default async function VoteForm({ voteId }: VoteFormProps) {
   return (
     <>
       <CardTitle>{data.title}</CardTitle>
-      <CardTitle className="mt-4">{data.description}</CardTitle>
+      <CardDescription className="mt-4">{data.description}</CardDescription>
       <form action={submitVoteWithParmas} className="mt-4">
-        {options?.map(({ id, value }) => (
-          <Label key={id}>
-            <Input type="radio" name="option" value={id} required />
-            {value}
-          </Label>
-        ))}
+        <ul className="flex flex-col gap-4">
+          {options?.map(({ id, value }) => (
+            <li key={id} className="flex justify-start items-center ">
+              <Input
+                className="hidden peer"
+                type="radio"
+                name="option"
+                value={id}
+                id={id}
+                required
+              />
+              <Label
+                htmlFor={id}
+                className="w-full bg-slate-white h-12 flex items-center pl-2 rounded-md  border-slate-300 hover:bg-slate-200 cursor-pointer border-2 text-slate-800 peer-checked:bg-slate-300"
+              >
+                {value}
+              </Label>
+            </li>
+          ))}
+        </ul>
         <Button type="submit" className="mt-4">
           투표 제출
         </Button>
