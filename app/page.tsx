20de77b37ec7c094f7c840c74641466a42aec2fb
ienrum <azang0506@gmail.com>
2024-05-27 11:00:32 +0900

    style: 시작 페이지 스타일 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1>일정 투표 사이트</h1>
-      <h3 className="mt-4">
+      <CardTitle>일정 투표 사이트</CardTitle>
+      <CardDescription className="mt-4">
         사용자가 간편하게 일정을 조율하고 투표할 수 있도록 돕는 서비스입니다.
-      </h3>
+      </CardDescription>
       <Link href="/create-vote">
         <Button className="mt-4">투표 생성</Button>
       </Link>
