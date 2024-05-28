9ca66691f09e1fe31f511d3a40d838b611c4b2c4
ienrum <azang0506@gmail.com>
2024-05-28 20:24:52 +0900

    refactor: 안쓰는 import 제거

--- app/create-vote/page.tsx
+++ app/create-vote/page.tsx
@@ -1,5 +1,4 @@
 import VoteForm from "@/app/create-vote/_components/VoteForm";
-import { createClient } from "@/lib/supabase/server";
 
 export default async function CreateVote() {
   return <VoteForm />;
